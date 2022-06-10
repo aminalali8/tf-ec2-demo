29a71e14f735ff6f148c0159e076b1c99dceb4e6
Amin Al Ali Al Darwish <amin@bunnyshell.com>
2022-06-10 23:16:27 +0300

    update module

--- main.tf
+++ main.tf
@@ -1,98 +1,62 @@
 provider "aws" {
-  region = local.region
-}
-
-locals {
-  # name   = "example-ec2-complete"
-  name   = var.instance_name
   region = "eu-west-1"
-
-  user_data = <<-EOT
-  #!/bin/bash
-  echo "Hello Terraform!"
-  EOT
-
-  tags = {
-    Owner       = "user"
-    Environment = "dev"
-  }
 }
 
-################################################################################
-# Supporting Resources
-################################################################################
+##################################################################
+# Data sources to get VPC, subnet, security group and AMI details
+##################################################################
+data "aws_vpc" "default" {
+  default = true
+}
 
-module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "~> 3.0"
-
-  name = local.name
-  cidr = "10.99.0.0/18"
-
-  azs              = ["${local.region}a", "${local.region}b", "${local.region}c"]
-  public_subnets   = ["10.99.0.0/24", "10.99.1.0/24", "10.99.2.0/24"]
-  private_subnets  = ["10.99.3.0/24", "10.99.4.0/24", "10.99.5.0/24"]
-  database_subnets = ["10.99.7.0/24", "10.99.8.0/24", "10.99.9.0/24"]
-
-  tags = local.tags
+data "aws_subnet_ids" "all" {
+  vpc_id = "${data.aws_vpc.default.id}"
 }
 
 data "aws_ami" "amazon_linux" {
   most_recent = true
-  owners      = ["amazon"]
 
   filter {
-    name   = "name"
-    values = ["amzn-ami-hvm-*-x86_64-gp2"]
+    name = "name"
+
+    values = [
+      "amzn-ami-hvm-*-x86_64-gp2",
+    ]
+  }
+
+  filter {
+    name = "owner-alias"
+
+    values = [
+      "amazon",
+    ]
   }
 }
 
 module "security_group" {
-  source  = "terraform-aws-modules/security-group/aws"
-  version = "~> 4.0"
+  source = "terraform-aws-modules/security-group/aws"
 
-  name        = local.name
+  name        = "example"
   description = "Security group for example usage with EC2 instance"
-  vpc_id      = module.vpc.vpc_id
+  vpc_id      = "${data.aws_vpc.default.id}"
 
   ingress_cidr_blocks = ["0.0.0.0/0"]
   ingress_rules       = ["http-80-tcp", "all-icmp"]
   egress_rules        = ["all-all"]
-
-  tags = local.tags
 }
 
-resource "aws_placement_group" "web" {
-  name     = local.name
-  strategy = "cluster"
+resource "aws_eip" "this" {
+  vpc      = true
+  instance = "${module.ec2.id[0]}"
 }
 
-resource "aws_kms_key" "this" {
-}
+module "ec2" {
+  source = "../../"
 
-resource "aws_network_interface" "this" {
-  subnet_id = element(module.vpc.private_subnets, 0)
-}
-
-################################################################################
-# EC2 Module
-################################################################################
-
-module "ec2_instance" {
-  source  = "terraform-aws-modules/ec2-instance/aws"
-  version = "~> 3.0"
-
-  name = locals.name
-
-  ami                    = "ami-ebd02392"
-  instance_type          = "t2.micro"
-  key_name               = "user1"
-  monitoring             = true
-  vpc_security_group_ids = ["sg-12345678"]
-  subnet_id              = "subnet-eddcdzz4"
-
-  tags = {
-    Terraform   = "true"
-    Environment = "dev"
-  }
+  name                        = var.instance_name
+  ami                         = "${data.aws_ami.amazon_linux.id}"
+  instance_type               = "t2.micro"
+  subnet_id                   = "${element(data.aws_subnet_ids.all.ids, 0)}"
+  vpc_security_group_ids      = ["${module.security_group.this_security_group_id}"]
+  associate_public_ip_address = true
 }
